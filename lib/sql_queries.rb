a0d98643a82a67ade10824caf140d0df8f02ae2f
Samantha brake <sammy.brake@gmail.com>
2018-11-03 20:19:27 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,15 +17,15 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT Projects.title, Projects.funding_goal, SUM(Pledges.amount - Projects.funding_goal) FROM projects INNER JOIN pledges ON Projects.id = Pledges.project_id GROUP BY Projects.title HAVING SUM(Pledges.amount - Projects.funding_goal) >=0)";
+"SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal FROM projects INNER JOIN pledges ON Projects.id = Pledges.project_id GROUP BY Projects.title HAVING SUM(Pledges.amount) - Projects.funding_goal >= 0";
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT Users.name, SUM(Pledges.amount) FROM users INNER JOIN pledges WHERE Users.id = Pledges.user_id GROUP BY Users.name ORDER BY SUM(Pledges.amount)";
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+""
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
