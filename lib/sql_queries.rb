3ed2c2a93eaee178de47e99e5e5a6f7d1b27e04a
Samantha brake <sammy.brake@gmail.com>
2018-11-03 20:39:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,5 +31,5 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT Projects.category FROM projects WHERE Projects.category = 'books'";
+"SELECT Projects.category, SUM(Pledges.amount) FROM projects JOIN pledges WHERE Projects.id = Pledges.project_id AND Projects.category = 'books'";
 end
